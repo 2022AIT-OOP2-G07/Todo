5c73063dd024de21a6a3b31974ff80aeeab4c52c
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2022-12-30 23:19:34 +0900

    メニューバーアプリの基盤

---
+++ menubar.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+import rumps
+
+
+class HelloApp(rumps.App):
+    pass
+
+if __name__ == "__main__":
+    HelloApp("HelloApp", icon="icon/fois.png", quit_button="終了").run()
