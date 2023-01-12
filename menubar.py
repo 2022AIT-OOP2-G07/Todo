040581b6d2cef0079fede6a06cc41eca74a0043f
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2023-01-12 16:03:01 +0900

    通知機能（途中）

--- menubar.py
+++ menubar.py
@@ -4,7 +4,35 @@ import rumps
 
 
 class HelloApp(rumps.App):
-    pass
+    @rumps.clicked("Start Timer!")
+    def timer(self, _):
+        # 一定時間ごとに処理を実行するタイマー
+
+        count = 0
+
+        # callback関数は引数にTimerオブジェクトをとる
+        def counter(t):
+            nonlocal count
+            count += 1
+            print(count)
+            if count >= 10:
+                # print("Stop Timer!")
+                self.notification()
+                t.stop()
+
+        # タイマーオブジェクト
+        # 一定時間ごとにcallbackを呼び出す
+        timer = rumps.Timer(callback=counter, interval=1)
+        timer.start()
+
+class GetData():
+    def getData():
+        data=datetime.datetime.now()
+        print(data)
+        # return data
 
 if __name__ == "__main__":
+    get = GetData()
+    get.getData()
+    # print(get.getData())
     HelloApp("HelloApp", icon="icon/fois.png", quit_button="終了").run()
