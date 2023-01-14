09f030f7710885d058fa69b4722e2b59832099b9
manaka3100 <119583257+manaka3100@users.noreply.github.com>
2023-01-14 17:39:15 +0900

    todo_list.pyをmain.pyに変更
    ・todo_list.pyの名前をmain.pyに変更
    ・todo.phpの削除
    ・todo.htmlの名前をindex.htmlに変更
    しました。
    この状態から実装を始めたいです。

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-# http://127.0.0.1:5000/address
-@app.route('/', methods=["GET"])
-def todo_get():
-
-    # パラメータの取得
-    #yotei = request.args.get('schedule-input', None)
-    #datetime = request.args.get('datetime-input', None)
-    
-    return render_template("todo.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# ０．sqlite3をインポート。これがないと始まらない
+import sqlite3
+# １．DB接続。ファイルがなければ作成する
+con = sqlite3.connect('todo_list.db')
+# ２．テーブル作成
+con.execute("CREATE TABLE IF NOT EXISTS todo(id integer PRIMARY KEY, todo_data text,todo_time datetime default (DATETIME('now','locatetime')), todo_deadline datetime, check_data boolean)")
+# ３．テーブルにデータを追加
+con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(1,'ご飯を食べる','2023-01-12 19:00',false)")
+con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(2,'勉強をする','','2023-01-23 17:00',false)")
+con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(3,'おせちを食べる','2023-01-01 12:00',true")
+con.commit()
 
 
+# 5．データ削除
+##con.execute("DELETE FROM todo WHERE id = 1")
+##con.execute("DELETE FROM todo WHERE id = 2")
+##con.execute("DELETE FROM todo WHERE id = 3")
+con.commit()
+# ４．データ参照
+cur = con.execute("SELECT * FROM todo")
+for row in cur:
+    print(row)
+    print(type(row))
+cur.close()
 
+# 6．テーブル削除
+##con.execute("DROP TABLE todo")
+# 7．DB接続解除
+con.close()
