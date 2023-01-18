c55f9b246b0d667afda8361a388ce1debec7d490
manaka3100 <119583257+manaka3100@users.noreply.github.com>
2023-01-18 21:16:54 +0900

    todo追加機能　途中
    JavaScriptからPythonに入力したtodoなどのデータを渡す機能までやりました！

--- static/todo.js
+++ static/todo.js
@@ -39,3 +39,69 @@ document.querySelectorAll("input[id^=fav-]").forEach((elm) => {
     })
 })
 
+document.getElementById("add-submit").addEventListener("click", async (ev) => {
+    // ボタンイベントのキャンセル
+    ev.preventDefault()
+    console.log("addボタン押されたよ！！")
+
+    // 入力チェック
+    let todo = document.getElementById("todo").value
+    let limit = document.getElementById("limit").value
+    console.log(todo)
+    console.log(limit)
+
+    // 未入力がある項目ごとにエラーメッセージを積み上げる
+    let error_message = ""
+    if (!todo) error_message += "予定が未入力です。<br>"
+    if (!limit) error_message += "期限が未入力です。<br>"
+    
+
+    // エラーメッセージがあるかどうかでエラーの表示有無を決定
+    if (error_message !== "") {
+        let err = document.getElementById('error-container')
+        err.innerHTML = error_message
+        document.getElementById('error-container').style.display = "table"
+        return
+    } else {
+        document.getElementById('error-container').innerHTML = ""
+        document.getElementById('error-container').style.display = "none"
+    }
+
+    //main.pyにデータを渡す
+    const param = new FormData()
+    param.append("todo", todo)
+    param.append("limit", limit)
+    console.log(param)
+    //console.log(param.limit)
+
+    fetch('/register_done', {
+        method: 'POST',
+        body: param,
+    }).then((response) => {
+        console.log(response)
+       
+        //入力項目の初期化
+        document.getElementById("schedule").reset()
+
+        // エラーの表示領域を初期化
+        document.getElementById('error-container').innerHTML = ""
+        document.getElementById('error-container').style.display = "none"
+        // 登録メッセージ等の表示領域を初期化
+        document.getElementById('message-container').innerHTML = ""
+        document.getElementById('message-container').style.display = "none"
+
+
+        response.json().then((data) => {
+            console.log(data) // 取得されたレスポンスデータをデバッグ表示
+            
+            // 正常にタスクを完了できているかを確認
+            if (data.result !== "ok") {
+                window.alert(data.message) // エラー表示
+            } else {
+                // タスクを完了できたら、チェックされた行を消す
+                window.alert(data.message)
+            }
+        })
+        
+    })
+})
