8574269f12f8fb103c4292b07865af079b438de9
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2023-01-19 20:22:30 +0900

    通知ボタンを押すことで一番近い予定の期限になったら通知を送ってくれる
    【課題】
    ・アプリ起動時に自動で通知機能を発動
    　　ーrumpsを起動している際にsleep関数を起動できない
    【解決策】
    ・アプリを分けて別アプリで通知だけの機能を持たせる

--- todo_list.py
+++ todo_list.py
@@ -5,10 +5,7 @@ con = sqlite3.connect('todo_list.db')
 # ２．テーブル作成
 
 con.execute("CREATE TABLE IF NOT EXISTS todo(id integer PRIMARY KEY, todo_data text, todo_deadline datetime, check_data boolean)")
+# (通知ボタンを押すことで一番近い予定の期限になったら通知を送ってくれる)
 
 # 3．データ参照
 cur = con.execute("SELECT * FROM todo")
-for row in cur:
-    print(row)
-    print(type(row))
-cur.close()
