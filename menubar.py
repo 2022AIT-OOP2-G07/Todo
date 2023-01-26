89facd98e923eb2760ec1583c48702ddb3c46905
hapiharu012 <120043995+hapiharu012@users.noreply.github.com>
2023-01-27 03:02:16 +0900

    通知オンの状態でもアプリが操作できる様になった
    【課題】
    通知するデータとメニューバーに表示するデータは配列は同じものを使用し、index情報だけを別で管理できる様にする。

--- menubar.py
+++ menubar.py
@@ -1,103 +1,64 @@
-#1/24 04:17時点で最新ver
-#実行可
+# 1/26 21:11時点で最新ver
+# 実行可
 
 import datetime
 import time
 import rumps
 import sqlite3
-# import threading
 
 
-index=0
-flag=True
+index = 0
+flag = True
+
+data=[]
+
+def getData():
+    global data
+    con = sqlite3.connect('todo_list.db')  # データベースに接続
+    cur = con.execute("select * from todo where todo_deadline <> 1 order by todo_deadline")  # 昇順にデータを取り出し
+    # [(1, 'ご飯を食べる', '2023-01-24 02:45', 0)]　ー＞時間を取り出し
+    z=cur.fetchall()
+    if data!= z and len(data)!=0:
+        print("------------------------")
+        print("⭐️データが更新されました⭐️")
+        print("------------------------")
+    data = z
+    
+    cur.close()
+    con.close()
+
+# def check_menubar():
+#     global index,data
+#     for el in data:
+#         if
+
+# def check_tuuti():
+#     global index,data
+#     for el in data:
+#         if
+
+
+
 class MenuBar(rumps.App):
-    def __init__(self):
-        global index
-        cur = con.execute("select * from todo where todo_deadline <> 1 order by todo_deadline")
-        # print(cur.fetchall()[0][1])
-        super(MenuBar, self).__init__(name="メニューバーtodo",title=cur.fetchall()[index][1],icon=None)
-    # print(cur1.fetchall())
-    # print(cur1.fetchall())
-        cur.close()
-        con.close()
-    print("a")
 
-    @rumps.clicked("通知オン")
-    def reload(self, _):
-        self.startTrigar()
+    def __init__(self):
+        global index,data
+        getData()
+        print(data)
+        # for 
+        super(MenuBar, self).__init__(name="メニューバーtodo",title=data[index][1], icon=None)
+        print("アプリ起動-no problem")
     
 
+    
 
-    def startTrigar(self):#aaa
-        global index
-        global flag
-
-        print("aaa")
-        con = sqlite3.connect('todo_list.db')   #データベースに接続
-        cur1 = con.execute("select * from todo where todo_deadline <> 1 order by todo_deadline")    #昇順にデータを取り出し
-        siteizikan=cur1.fetchall()[index][2]    #[(1, 'ご飯を食べる', '2023-01-24 02:45', 0)]　ー＞時間を取り出し
-        cur2 = con.execute("select * from todo where todo_deadline <> 1 order by todo_deadline")
-        todo=cur2.fetchall()[index][1]  #todo内容の取り出し
-
-        data=datetime.datetime.now()
-        exec_time = datetime.datetime.strptime(siteizikan ,'%Y-%m-%d %H:%M')    #取り出した期限の時間（siteizikan）は文字列だからdateオブジェクトに変換
-
-        # print(type(data))
-
-        print("-----------------------------------")
-        print('現在時刻：' , data)
-        print('実行時間：' , exec_time , 'まで待機します。')
-        print("-----------------------------------")
-
-        sleep_time = exec_time - data   #(期限)ー(現在時刻)でタイマー時間を算出
-
-        # print(type(sleep_time.total_seconds()))
-        print(f'ーー{sleep_time}秒後に通知するよーー')
-        
-        try:
-            time.sleep(sleep_time.total_seconds())
-            try:
-                
-                print('実行時間になりました')
-                print('現在時刻：' , datetime.datetime.now())
-
-                index+=1
-                cur2 = con.execute("select * from todo where todo_deadline <> 1 order by todo_deadline")
-                try:
-                    
-                    next_todo=cur2.fetchall()[index][1]
-                    print("-------------")
-                    print(f'次の予定は[{next_todo}]です。')
-                    print(f'index:{index}')
-
-                    flag=True
-                    print(f'flag:{flag}')
-                    print("-------------")
-                    app.title=next_todo
-                except:
-                    flag=False
-                    print("次の予定はありません")
-                
-                
-                self.tuuti(todo)
-
-            except  Exception:
-                print("通知関数を呼び出せませんでした。")
-
-        except  Exception:
-            print("🟨期限が過ぎているため通知登録ができません。")
-        
-        
-
-
-
-    def tuuti(self,todo):
+    def tuuti(self, todo):
         global flag
 
         show_text = f"現在時刻は[{datetime.datetime.now()}]です。お疲れ様でした。"
-        text= f"「{todo}」の期限が終了しました。"
-        rumps.notification(     #通知
-            todo,   #todo内容
+        text = f"「{todo}」の期限が終了しました。"
+        rumps.notification(  # 通知
+            todo,  # todo内容
             text,
             show_text,
             # icon="fois.png",
@@ -105,21 +66,78 @@ class MenuBar(rumps.App):
         # if flag==True:
         #     self.startTrigar()
 
+    @rumps.clicked("通知オン")
+    def timer(self, _):
+        
+        def trigar(t):
+            global index
+            global flag,data
+            
+            getData()
+            print(data)
+            siteizikan = data[index][2]  # [(1, 'ご飯を食べる', '2023-01-24 02:45', 0)]　->時間を取り出し
+            todo = data[index][1]  # todo内容の取り出し
+
+            siteizikan_date = datetime.datetime.strptime(siteizikan, '%Y-%m-%d %H:%M')  # 取り出した期限の時間（siteizikan）は文字列だからdateオブジェクトに変換
+            siteizikan_format=siteizikan_date.strftime("%Y-%m-%d %H:%M")    #dateオブジェクトに変換したsiteizikan_dateを"%Y-%m-%d %H:%M"にフォーマット
+            now = datetime.datetime.now()
+            now_format=now.strftime("%Y-%m-%d %H:%M")
+            print(f"sitei:{siteizikan_format}")
+            print(f"now:{now_format}")
+            
+            print("a")
+            if siteizikan_format == now_format:
+                
+                try:
+                    print(f"{todo}の時間だよ！")
+                    print('現在時刻：', datetime.datetime.now())
+                    index += 1
+                    
+                    try:
+                        next_todo = data[index][1]
+                        print("-------------")
+                        print(f'次の予定は[{next_todo}]です。')
+                        print(f'index:{index}')
+                        
+                        flag = True
+                        print(f'flag:{flag}')
+                        print("-------------")
+                        app.title = next_todo
+                    except:
+                        flag = False
+                        print("次の予定はありません")
+                        t.stop()
+                    
+                except Exception:
+                    print("通知関数を呼び出せませんでした。")
+                self.tuuti(todo)    #検査のためにtry文だしてる
+                
+
+        
+        timer = rumps.Timer(callback=trigar, interval=1)
+        timer.start()
+        
+    # @rumps.clicked("更新")
+    # def timer(self, _):
+    #     pass
+
 
 if __name__ == "__main__":
     con = sqlite3.connect('todo_list.db')
-        
-    con.execute("CREATE TABLE IF NOT EXISTS todo(id integer PRIMARY KEY, todo_data text, todo_deadline datetime, check_data boolean)")
+
+    con.execute(
+        "CREATE TABLE IF NOT EXISTS todo(id integer PRIMARY KEY, todo_data text, todo_deadline datetime, check_data boolean)")
     # # # # ３．テーブルにデータを追加
-    con.execute("DELETE FROM todo WHERE id = 1") 
-    con.execute("DELETE FROM todo WHERE id = 2") 
-    con.execute("DELETE FROM todo WHERE id = 3") 
-    con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(1,'ご飯を食べる','2023-01-24 03:59',false)")
-    con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(2,'勉強をする','2023-01-24 04:00',false)")
-    con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(3,'おせちを食べる','2023-01-24 04:01',true)")
-    # con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(1,'今すぐやれ','2023-01-19 20:05',true)")
+    con.execute("DELETE FROM todo WHERE id = 1")
+    # con.execute("DELETE FROM todo WHERE id = 2")
+    # con.execute("DELETE FROM todo WHERE id = 3")
+    # con.execute("DELETE FROM todo WHERE id = 4")
+
+    # con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(4,'4＿課題提出','2023-01-26 21:42',false)")
+    # con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(2,'2＿朝ごはんだよ','2023-01-26 21:40',false)")
+    # con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(3,'3＿出る時間だよ','2023-01-26 21:41',true)")
+    con.execute("INSERT INTO todo(id, todo_data, todo_deadline, check_data)values(1,'1＿起きるよ','2023-01-27 02:43',true)")
     con.commit()
 
-    app=MenuBar()
+    app = MenuBar()
     app.run()
-    
